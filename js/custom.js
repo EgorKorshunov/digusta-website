514b2eed33f089f492ba48cdd54b3f858b60875a
Egor Korshunov <korshunoffegor@gmail.com>
2021-02-21 20:49:11 +0100

    Website upload

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+let mainModel = document.querySelector("#model-box>img");
+let mainDish = document.querySelector("#dish-box>img");
+let dishes = document.querySelectorAll(".dish-image-small");
+let models = document.querySelectorAll(".model-image-small");
+let navbar = document.querySelector(".navbar");
+let navbarButton = document.querySelector("nav>button");
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const changeMainImage = (img) => {
+    mainDish.src = img.src;
+}
+
+
+dishes.forEach(item => item.addEventListener("click", () => changeMainImage(item)));
+
+models.forEach(model => {
+    model.addEventListener("click", e => {
+        lightbox.classList.add('active');
+        const bigImg = document.createElement('iframe');
+        bigImg.classList.add('big-model');
+        console.log("model", model.src);
+        console.log("panorama", model.getAttribute("data-panorama"));
+        bigImg.src = model.getAttribute("data-panorama");
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(bigImg);
+    })
+})
+
+lightbox.addEventListener("click", e => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active');
+})
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  if (!navbarButton.classList.contains("collapsed")) return;
+  const windowHeight = window.innerHeight;
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.classList.remove("navbar-hide");
+    navbar.classList.add("navbar-unhide");
+  } else { 
+    navbar.classList.remove("navbar-unhide");
+    navbar.classList.add("navbar-hide");
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// window.addEventListener("scroll")
